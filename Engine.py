2150beb18460498280359fc1c72b64ed931acf54
Paul <WildZes@gmail.com>
2021-03-06 16:22:07 +0300

    Point 3.1 from algorithm partialy realized. Different changes in mq4 file to create csv file to work with and get DataFrame.

--- Engine.py
+++ Engine.py
@@ -1,34 +1,83 @@
 from DWX_ZeroMQ_Connector_v2_0_1_RC8 import DWX_ZeroMQ_Connector
-import sys
-from io import StringIO
 from time import sleep
+from io import StringIO
+import os
+import sys
+
+#Define trend direction
+def trend_dir(quotes):
+    import pandas as pd
+    import matplotlib.dates as mdates
+    import numpy as np
+    y = np.array(quotes[['Open', 'High', 'Low', 'Close']]).ravel()
+    x = np.array(quotes.index).ravel()
+    x = np.array([(lambda d: mdates.date2num(d))(d) for d in x for _ in (0,1,2,3)])
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+    if m < 0:
+        _dir = -1
+    elif m > 0:
+        _dir = 1
+    else:
+        _dir = 0 #have to organize check for sideway trend somehow (28.02.2021 update: orginized via get_ength and calculating median for it)
+    return _dir
+
+
+#Dataframe creation
+def get_df():
+    import pandas as pd
+    cur_path = r'C:\Users\mrP\AppData\Roaming\MetaQuotes\Terminal\C9EACFC244DF3C49B263EE60244932A4\MQL4\Files\data.csv'
+    if os.path.isfile(cur_path):
+        col_names=['DateTime','Open','High','Low','Close','Tick_Vol']
+        df = pd.read_csv (cur_path, names=col_names)
+        df['DateTime']=pd.to_datetime(df.pop('DateTime'))
+        my_df = df.set_index('DateTime')
+        return my_df
 
 
 #Function to get oped as int
-def foped():
-    try:
-        sys.stdout = result
-        _zmq._SPI_ZMQ_GET_OPED_()
-        sleep(0.2)
-        out = result.getvalue()
-        sys.stdout = origin_stdout
-        return int(out)
-    except Exception as e:
-        return e.args
+def f_get(command):
+    sys.stdout = result
+    command()
+    sleep(0.2)
+    out = result.getvalue()
+    sys.stdout = origin_stdout
+    return out
 
 
 #On start (no open orders)
-def run(open_orders):
-    if ~isinstance(open_orders, int):
-        raise NameError(open_orders)
-    while open_orders < 4:
-        pass
-    re_run()
+def run(oped):
+    if 'Error' in oped:
+        raise NameError(oped)
+    if 'OPEN_TRADES' in oped:
+        return re_run()
+    try:
+        openCurList = f_get(_zmq._DWX_MTX_GET_ALL_OPEN_TRADES_)
+        while int(oped) < 4:
+            arrSymbol = [ok for ok in currencyList
+                         if ok in openCurList]
+            for cur in currencyList:
+                if cur in arrSymbol:
+                    continue
+                _zmq._DWX_MTX_SEND_MARKETDATA_REQUEST_(_symbol=cur, _timeframe=240,
+                                                       _start='2020.1.1', _end='2020.1.2')
+                sleep(0.3)
+                df_H4 = get_df()
+                _zmq._DWX_MTX_SEND_MARKETDATA_REQUEST_(_symbol=cur, _timeframe=1440,
+                                                       _start='2020.1.1', _end='2020.1.2')
+                sleep(0.3)
+                df_D1 = get_df()
+                if trend_dir(df_H4) != trend_dir(df_D1):
+                    continue              
+        oped = f_get(_zmq._SPI_ZMQ_GET_OPED_())
+        return re_run()
+    except Exception as e:
+        raise NameError(e.args)
 
 #To wait changes
 def re_run():
-    sleep(1440)
-    run(foped())
+    sleep(1) #1440 for 4 hours
+    return run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
 
 
 currencyList = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD',
@@ -38,11 +87,10 @@ timeframeList = ['1', '5', '15', '30', '60', '240', '1440', '10080']
 origin_stdout = sys.stdout
 result = StringIO()
 _zmq = DWX_ZeroMQ_Connector()
-oped = foped()
 
 if __name__ == "__main__":
     try:
-        run(foped())
+        run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
     except KeyboardInterrupt:
         sys.stdout = origin_stdout
     except NameError:
