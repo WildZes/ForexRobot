289d895d6ad1539a49c9a515fefa2dd99514aee5
Paul <WildZes@gmail.com>
2021-03-15 21:26:06 +0300

    Engine.py and MT4_calculations.py partialy tested.

--- Engine.py
+++ Engine.py
@@ -1,45 +1,16 @@
 from DWX_ZeroMQ_Connector_v2_0_1_RC8 import DWX_ZeroMQ_Connector
 from time import sleep
 from io import StringIO
-import os
 import sys
-
-#Define trend direction
-def trend_dir(quotes):
-    import pandas as pd
-    import matplotlib.dates as mdates
-    import numpy as np
-    y = np.array(quotes[['Open', 'High', 'Low', 'Close']]).ravel()
-    x = np.array(quotes.index).ravel()
-    x = np.array([(lambda d: mdates.date2num(d))(d) for d in x for _ in (0,1,2,3)])
-    A = np.vstack([x, np.ones(len(x))]).T
-    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-    if m < 0:
-        _dir = -1
-    elif m > 0:
-        _dir = 1
-    else:
-        _dir = 0 #have to organize check for sideway trend somehow (28.02.2021 update: orginized via get_ength and calculating median for it)
-    return _dir
-
-
-#Dataframe creation
-def get_df():
-    import pandas as pd
-    cur_path = r'C:\Users\mrP\AppData\Roaming\MetaQuotes\Terminal\C9EACFC244DF3C49B263EE60244932A4\MQL4\Files\data.csv'
-    if os.path.isfile(cur_path):
-        col_names=['DateTime','Open','High','Low','Close','Tick_Vol']
-        df = pd.read_csv (cur_path, names=col_names)
-        df['DateTime']=pd.to_datetime(df.pop('DateTime'))
-        my_df = df.set_index('DateTime')
-        return my_df
+import MT4_calculations as MT4
 
 
 #Function to get oped as int
 def f_get(command):
+    result = StringIO()
     sys.stdout = result
     command()
-    sleep(0.2)
+    sleep(0.5)
     out = result.getvalue()
     sys.stdout = origin_stdout
     return out
@@ -52,8 +23,8 @@ def run(oped):
     if 'OPEN_TRADES' in oped:
         return re_run()
     try:
-        openCurList = f_get(_zmq._DWX_MTX_GET_ALL_OPEN_TRADES_)
-        while int(oped) < 4:
+        while int(oped[0]) < 4:
+            openCurList = f_get(_zmq._DWX_MTX_GET_ALL_OPEN_TRADES_)
             arrSymbol = [ok for ok in currencyList
                          if ok in openCurList]
             for cur in currencyList:
@@ -61,38 +32,79 @@ def run(oped):
                     continue
                 _zmq._DWX_MTX_SEND_MARKETDATA_REQUEST_(_symbol=cur, _timeframe=240,
                                                        _start='2020.1.1', _end='2020.1.2')
-                sleep(0.3)
-                df_H4 = get_df()
+                sleep(0.5)
+                df_H4 = MT4.get_df()
                 _zmq._DWX_MTX_SEND_MARKETDATA_REQUEST_(_symbol=cur, _timeframe=1440,
                                                        _start='2020.1.1', _end='2020.1.2')
-                sleep(0.3)
-                df_D1 = get_df()
-                if trend_dir(df_H4) != trend_dir(df_D1):
-                    continue              
-        oped = f_get(_zmq._SPI_ZMQ_GET_OPED_())
-        return re_run()
+                sleep(0.5)
+                df_D1 = MT4.get_df()
+                dir_H4 = MT4.trend_dir(df_H4)
+                dir_D1 = MT4.trend_dir(df_D1)
+                if (dir_H4 != dir_D1)or(dir_H4 == 0)or(dir_D1==0):
+                    continue
+                RSI = MT4.RSI(df_H4['Close'], len(df_H4['Close'])-1)
+                if (RSI[0] > 70) or (RSI[0] < 30):
+                    continue
+                a, b = [], []
+                for tf in timeframeList:
+                    _zmq._DWX_MTX_SEND_MARKETDATA_REQUEST_(_symbol=cur, _timeframe=tf,
+                                                       _start='2020.1.1', _end='2020.1.2')
+                    sleep(2)
+                    any_df = MT4.get_df()
+                    m, v, a, b = MT4.get_m_v(a, b, any_df)
+                if (df_H4['Tick_Vol'][0] < v) or (MT4.get_ength(df_H4) < m):
+                    continue
+                if dir_H4 < 0:
+                    _my_trade['_type']=1
+                    _my_trade['_symbol']=cur
+                    _zmq._DWX_MTX_NEW_TRADE_(_order=_my_trade)
+                    sleep(5)
+                    oped = f_get(_zmq._SPI_ZMQ_GET_OPED_)
+                    sleep(0.2)                    
+                    break
+                if dir_H4 > 0:
+                    _my_trade['_type']=0
+                    _my_trade['_symbol']=cur
+                    _zmq._DWX_MTX_NEW_TRADE_(_order=_my_trade)
+                    sleep(5)
+                    oped = f_get(_zmq._SPI_ZMQ_GET_OPED_)
+                    sleep(0.2)
+                    break
+                if (cur == 'NZDCAD') and (int(oped[0]) < 4):
+                    return re_run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
+        return re_run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
     except Exception as e:
         raise NameError(e.args)
 
 #To wait changes
-def re_run():
-    sleep(1) #1440 for 4 hours
-    return run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
+def re_run(oped):
+    while (int(oped[0]) >= 4):
+        print('Sleeping for 4 hour...')
+        sleep(14400) #14400 for 4 hours
+        oped = f_get(_zmq._SPI_ZMQ_GET_OPED_)
+    if int(oped[0]) < 4:
+        print('Sleeping for 1 hours...')
+        sleep(3600)
+        oped = f_get(_zmq._SPI_ZMQ_GET_OPED_)
+    return run(oped)
 
 
 currencyList = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD',
                 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPCHF', 'GBPJPY', 'GBPUSD', 'NZDJPY', 'NZDUSD',
                 'USDCAD', 'USDCHF', 'USDJPY', 'GBPAUD', 'GBPNZD', 'GBPCAD', 'NZDCHF', 'NZDCAD']
-timeframeList = ['1', '5', '15', '30', '60', '240', '1440', '10080']
+timeframeList = [1, 5, 15, 30, 60, 240, 1440, 10080]
 origin_stdout = sys.stdout
-result = StringIO()
 _zmq = DWX_ZeroMQ_Connector()
+_my_trade = _zmq._generate_default_order_dict()
+_my_trade['_SL']=400
+_my_trade['_TP']=600
 
 if __name__ == "__main__":
     try:
         run(f_get(_zmq._SPI_ZMQ_GET_OPED_))
     except KeyboardInterrupt:
         sys.stdout = origin_stdout
+        print('Ctrl+C: success')
     except NameError:
         raise
     finally:
---
+++ MT4_calculations.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.dates as mdates
+import numpy as np
+import os.path as op
+
+
+#Define trend direction
+def trend_dir(quotes):
+    y = np.array(quotes[['Open', 'High', 'Low', 'Close']]).ravel()
+    x = np.array(quotes.index).ravel()
+    x = np.array([(lambda d: mdates.date2num(d))(d) for d in x for _ in (0,1,2,3)])
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+    if m < 0:
+        _dir = -1
+    elif m > 0:
+        _dir = 1
+    else:
+        _dir = 0 #have to organize check for sideway trend somehow (28.02.2021 update: orginized via get_ength and calculating median for it)
+    return _dir
+
+
+#Dataframe creation
+def get_df():
+    cur_path = r'C:\Users\mrP\AppData\Roaming\MetaQuotes\Terminal\C9EACFC244DF3C49B263EE60244932A4\MQL4\Files\data.csv'
+    if op.isfile(cur_path):
+        col_names=['DateTime','Open','High','Low','Close','Tick_Vol']
+        df = pd.read_csv (cur_path, names=col_names)
+        df['DateTime']=pd.to_datetime(df.pop('DateTime'))
+        my_df = df.set_index('DateTime')
+        return my_df
+    raise NameError('get_df is broken')
+
+
+#Function gets m that is calculated the same way as it was calculated in treng_dir function
+def get_ength(quotes):
+    _quotes = quotes
+    y = np.array(_quotes[['Open', 'High', 'Low', 'Close']]).ravel()
+    x = np.array(_quotes.index).ravel()
+    x = np.array([(lambda d: mdates.date2num(d))(d) for d in x for _ in (0,1,2,3)])
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+    return m
+
+
+#Function that helps to define change of trand direction (RSI < 70 and RSI > 30: OK)
+def RSI(series, period):
+    delta = series.diff().dropna()
+    u = delta * 0
+    d = u.copy()
+    u[delta > 0] = delta[delta > 0]
+    d[delta < 0] = -delta[delta < 0]
+    u[u.index[period-1]] = np.mean( u[:period] ) #first value is sum of avg gains
+    u = u.drop(u.index[:(period-1)])
+    d[d.index[period-1]] = np.mean( d[:period] ) #first value is sum of avg losses
+    d = d.drop(d.index[:(period-1)])
+    rs = u.ewm(com=period-1, adjust=False).mean() / d.ewm(com=period-1, adjust=False).mean()
+    return 100 - 100 / (1 + rs)
+
+#Function to estimate treshold strength and volume
+def get_m_v(strengthList, volumeList, all_df):
+    strengthList.append(abs(get_ength(all_df)))
+    tmp = all_df['Tick_Vol'].values
+    volumeList += tmp.tolist()
+    return np.median(strengthList), np.median(volumeList), strengthList, volumeList
